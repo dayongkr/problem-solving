4bb91aca056ce6a1a7c87b9bcb90115e26766b28
Dayong Lee <dayongkr@gmail.com>
2023-03-05 09:38:38 +0900

    [Silver III] Title: Blocks, Time: 140 ms, Memory: 116324 KB -BaekjoonHub

---
+++ 백준/Silver/24622. Blocks/Blocks.py
@@ -0,0 +1,26 @@
+n = int(input())
+str = [input() for _ in range(4)]
+strSet = set()
+for a in range(4):
+    for b in range(4):
+        if a in [b]:
+            continue
+        for c in range(4):
+            if c in [a, b]:
+                continue
+            for d in range(4):
+                if d in [a, b, c]:
+                    continue
+                for s1 in str[a]:
+                    strSet.add(s1)
+                    for s2 in str[b]:
+                        strSet.add(s1+s2)
+                        for s3 in str[c]:
+                            strSet.add(s1+s2+s3)
+                            for s4 in str[d]:
+                                strSet.add(s1+s2+s3+s4)
+for i in range(n):
+    if (input() in strSet):
+        print("YES")
+    else:
+        print("NO")
