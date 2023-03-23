dbc005405f7a6e51a368dacfcb8ab1428c1c874f
Dayong Lee <dayongkr@gmail.com>
2023-03-23 10:03:54 +0900

    [Bronze V] Title: Who have not handed in ?, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. Who have not handed in ？/Who have not handed in ？.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+arr = [0] * 30
+
+for i in range(28):
+    n = int(input())
+    arr[n-1] = n
+
+for i in range(30):
+    if arr[i] == 0:
+        print(i+1)
