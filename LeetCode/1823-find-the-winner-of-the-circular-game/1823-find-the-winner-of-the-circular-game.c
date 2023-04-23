2760da805b95df3c78ba8956a43fdd5cfe7c1d8a
Dayong Lee <dayongkr@gmail.com>
2023-04-23 21:22:21 +0900

    Time: 3 ms (94.12%), Space: 5.5 MB (85.29%) - LeetHub

--- LeetCode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.c
+++ LeetCode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.c
@@ -1,31 +1,9 @@
-typedef struct node {
-    int value;
-    struct node *next;
-} node;
+int helper(int n, int k) {
+    if (n == 1) return 0;
+    return (k + helper(n - 1, k)) % n;
+
+}
 
 int findTheWinner(int n, int k) {
-    int count = 0;
-    node *head = (node *) malloc(sizeof(node));
-    node *tail = head;
-    head->value = 1;
-    head->next = NULL;
-    for (int i = 2; i <= n; i++) {
-        tail->next = (node *) malloc(sizeof(node));
-        tail = tail->next;
-        tail->value = i;
-        tail->next = NULL;
-    }
-    while (head->next != NULL) {
-        for (int i = 0; i < k; i++) {
-            tail->next = head;
-            head = head->next;
-            if (i == k - 1) {
-                count++;
-                tail->next = NULL;
-                break;
-            }
-            tail = tail->next;
-        }
-    }
-    return head->value;
+    return helper(n, k) + 1;
 }
