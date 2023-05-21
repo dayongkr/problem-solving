c765bf4d71c6efced290762857d52c760237fad2
Dayong Lee <dayongkr@gmail.com>
2023-05-21 11:28:33 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4184 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
+    }
+
+    for(int i = 0; i < n-1; i++) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+    return 0;
+}
