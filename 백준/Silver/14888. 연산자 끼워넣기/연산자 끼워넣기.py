9ec319eb2da3884f7b6155229b0ca0d92e2c1ddd
Dayong Lee <dayongkr@gmail.com>
2023-06-20 14:54:04 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -8,28 +8,24 @@ op = list(map(int, input().split()))
 max_result = -1000000001
 min_result = 1000000001
 
-
-def rec(n, a, b, c, d, s):
+def rec(n, plus, minus, mul, div, prev_result):
     global max_result
     global min_result
     if n == N:
-        if s > max_result:
-            max_result = s
-        if s < min_result:
-            min_result = s
-    else:
-        for i in range(4):
-            if i == 0 and a > 0:
-                rec(n + 1, a - 1, b, c, d, s + A[n])
-            elif i == 1 and b > 0:
-                rec(n + 1, a, b - 1, c, d, s - A[n])
-            elif i == 2 and c > 0:
-                rec(n + 1, a, b, c - 1, d, s * A[n])
-            elif i == 3 and d > 0:
-                if s < 0:
-                    rec(n + 1, a, b, c, d - 1, -(-s // A[n]))
-                else:
-                    rec(n + 1, a, b, c, d - 1, s // A[n])
+        if prev_result > max_result:
+            max_result = prev_result
+        if prev_result < min_result:
+            min_result = prev_result
+        return
+
+    if plus:
+        rec(n + 1, plus - 1, minus, mul, div, prev_result + A[n])
+    if minus:
+        rec(n + 1, plus, minus - 1, mul, div, prev_result - A[n])
+    if mul:
+        rec(n + 1, plus, minus, mul - 1, div, prev_result * A[n])
+    if div:
+        rec(n + 1, plus, minus, mul, div - 1, int(prev_result / A[n]))
 
 
 rec(1, op[0], op[1], op[2], op[3], A[0])
