927a50e4c900126389383d92b50057fc8793f94b
Dayong Lee <dayongkr@gmail.com>
2023-06-24 13:42:37 +0900

    [Silver I] Title: 경로 찾기, Time: 232 ms, Memory: 34272 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -4,28 +4,13 @@ from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
-queue = deque()
-
 adj_arr = [list(map(int, input().split())) for _ in range(N)]
-result = [[0 for _ in range(N)] for _ in range(N)]
 
-for i in range(N):
-    output = [[0 for _ in range(N)] for _ in range(N)]
-    for j in range(N):
-        if adj_arr[i][j] == 1 and output[i][j] == 0:
-            output[i][j] = 1
-            result[i][j] = 1
-            queue.append((i, j))
-            while queue:
-                y, x = queue.popleft()
-                for k in range(N):
-                    if adj_arr[x][k] == 1 and output[x][k] == 0:
-                        output[x][k] = 1
-                        result[x][k] = 1
-                        queue.append((x, k))
-                        if output[i][k] == 0:
-                            output[i][k] = 1
-                            result[i][k] = 1
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if adj_arr[i][k] == 1 and adj_arr[k][j] == 1:
+                adj_arr[i][j] = 1
 
-for item in result:
+for item in adj_arr:
     print(*item)
