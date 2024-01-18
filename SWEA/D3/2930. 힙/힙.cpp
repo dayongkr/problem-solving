69c524c128026662ef43597fc00dc3739b9b6a29
Dayong Lee <dayongkr@gmail.com>
2024-01-18 14:29:42 +0900

    [D3] Title: 힙, Time: 647 ms, Memory: 13,928 KB -BaekjoonHub

--- SWEA/D3/2930. 힙/힙.cpp
+++ SWEA/D3/2930. 힙/힙.cpp
@@ -1,69 +1,35 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
-
-int queue[100001], queue_size = 0;
-
-void init() {
-    for (int & i : queue)
-        i = 0;
-    queue_size = 0;
-}
-
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-void push(int value) {
-    queue[++queue_size] = value;
-    int index = queue_size, parent = index / 2;
-    while (parent != 0 && queue[index] > queue[parent]) {
-        swap(queue[index], queue[parent]);
-        index = index / 2;
-        parent = index / 2;
-    }
-}
-
-int pop() {
-    if (queue_size == 0) return -1;
-    int result = queue[1], parent = 1, large = 1, left = 2, right = 3;
-    swap(queue[1], queue[queue_size--]);
-    while (true) {
-        if (left <= queue_size && queue[left] > queue[large]) large = left;
-        if (right <= queue_size && queue[right] > queue[large]) large = right;
-        if (large == parent) break;
-        swap(queue[parent], queue[large]);
-        parent = large;
-        left = parent * 2;
-        right = left + 1;
-    }
-    return result;
-}
+priority_queue<int> pq = priority_queue<int>();
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int test_case, T, N, type = 0, value;
-
     cin >> T;
     for (test_case = 1; test_case <= T; ++test_case) {
-        init();
+        while (!pq.empty())
+            pq.pop();
         cin >> N;
         cout << '#' << test_case;
         for (int i = 0; i < N; i++) {
             cin >> type;
             if (type == 1) {
                 cin >> value;
-                push(value);
+                pq.push(value);
             } else {
-                cout << ' ' << pop();
+                if (pq.empty())
+                    cout << " -1";
+                else {
+                    cout << ' ' << pq.top();
+                    pq.pop();
+                }
             }
         }
         cout << '\n';
     }
-
     return 0;
 }
