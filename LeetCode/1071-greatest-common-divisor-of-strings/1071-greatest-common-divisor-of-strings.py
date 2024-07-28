d0ab464bd30cc9d866ac809c756dd429a2330efa
Dayong Lee <dayongkr@gmail.com>
2024-07-28 15:29:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ LeetCode/1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,16 +1,12 @@
-class Solution:
-    def gcd(self, a: int, b: int) -> int:
-        if b == 0:
-            return a
-        else :
-            return gcd(b, a % b)
+from math import gcd
 
+class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        gcd = self.gcd(len(str1), len(str2))
-        prefix = str1[:gcd]
-        print(gcd)
-        for i in range(0, len(str1), gcd):
-            if str1[i : i + gcd] != prefix:
+        GCD = gcd(len(str1), len(str2))
+        prefix = str1[:GCD]
+        
+        for i in range(0, len(str1), GCD):
+            if str1[i : i + GCD] != prefix:
                 return ""
         for i in range(0, len(str2), gcd):
             if str2[i : i + gcd] != prefix:
