418f45d9c72d2d074cd958ecab566555abe73b08
Dayong Lee <dayongkr@gmail.com>
2024-07-28 14:40:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
+++ LeetCode/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -2,9 +2,8 @@ function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
     const max_candies = Math.max(...candies)
     const n = candies.length;
     const result = [];
-
-    for (let i = 0; i < n; i++) {
-        if (candies[i] + extraCandies >= max_candies) {
+    for(const candy of candies)
+        if (candy + extraCandies >= max_candies) {
             result.push(true)
         } else{
             result.push(false)
