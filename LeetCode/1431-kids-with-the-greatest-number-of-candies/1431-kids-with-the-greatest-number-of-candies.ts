150a0ca77e442c26ba8f35dc4b26bdd03e7af9c3
Dayong Lee <dayongkr@gmail.com>
2024-07-28 14:39:49 +0900

    Time: 56 ms (85.23%), Space: 52.9 MB (16.41%) - LeetHub

--- LeetCode/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
+++ LeetCode/1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -1,16 +1,15 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    let max_candies = 0;
+    const max_candies = Math.max(...candies)
     const n = candies.length;
-    const result:boolean[] = Array.from({ length: n }, () => false)
-
-    for (const candy of candies) {
-        max_candies = Math.max(max_candies, candy)
-    }
+    const result = [];
 
     for (let i = 0; i < n; i++) {
         if (candies[i] + extraCandies >= max_candies) {
-            result[i] = true
+            result.push(true)
+        } else{
+            result.push(false)
         }
     }
+
     return result;
 };
