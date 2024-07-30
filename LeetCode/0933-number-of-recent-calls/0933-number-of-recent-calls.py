621f4ea26da59bfe1732d899bac6ff0f79a7ec1e
Dayong Lee <dayongkr@gmail.com>
2024-07-30 11:34:00 +0900

    Time: 205 ms (29.69%), Space: 22.1 MB (62.49%) - LeetHub

--- LeetCode/0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ LeetCode/0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -3,13 +3,8 @@ from collections import deque
 class RecentCounter:
 
     def __init__(self):
-        self.queue = deque()
+        self.ls = []
 
     def ping(self, t: int) -> int:
-        self.queue.append(t)
-        limit = t - 3000
-
-        while self.queue[0] < limit:
-            self.queue.popleft()
-
-        return len(self.queue)
+        self.ls.append(t)
+        return len(self.ls) - bisect.bisect_left(self.ls, t - 3000)
