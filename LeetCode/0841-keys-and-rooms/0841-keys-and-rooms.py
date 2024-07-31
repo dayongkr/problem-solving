7d9b473baaf1420a18516a79a08f1f674fdec93f
Dayong Lee <dayongkr@gmail.com>
2024-07-31 22:26:43 +0900

    Time: 63 ms (70.56%), Space: 17 MB (40.59%) - LeetHub

--- LeetCode/0841-keys-and-rooms/0841-keys-and-rooms.py
+++ LeetCode/0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -2,19 +2,18 @@ class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         n = len(rooms)
         opens = 1
-        visited = [False for _ in range(n)]
         
-        visited[0] = True
         stack = [0]
+        visited = set(stack)
 
         while stack:
             top = stack.pop()
             keys = rooms[top]
 
             for key in keys:
-                if not visited[key]:
+                if key not in visited:
                     opens += 1
-                    visited[key] = True
+                    visited.add(key)
                     stack.append(key)
 
         return opens == n
