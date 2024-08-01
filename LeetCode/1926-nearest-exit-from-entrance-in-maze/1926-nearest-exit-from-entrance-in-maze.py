b7d529596cae667b84355a143c71a89e8042d684
Dayong Lee <dayongkr@gmail.com>
2024-08-01 16:33:02 +0900

    Time: 658 ms (59.91%), Space: 17 MB (81.85%) - LeetHub

--- LeetCode/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ LeetCode/1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -3,12 +3,13 @@ from collections import deque
 
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        queue = deque()
+        ROW, COL = len(maze) - 1, len(maze[0]) - 1
         direct = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        queue.append((entrance[0], entrance[1], 0))
-        maze[entrance[0]][entrance[1]] = "+"
-        ROW = len(maze) - 1
-        COL = len(maze[0]) - 1
+        row, col, step = *entrance, 0
+
+        queue = deque()
+        queue.append((row, col, 0))
+        maze[row][col] = "+"
 
         while queue:
             row, col, step = queue.popleft()
@@ -16,7 +17,7 @@ class Solution:
 
             for dr, dc in direct:
                 n_row, n_col = row + dr, col + dc
-                
+
                 if ROW >= n_row >= 0 and COL >= n_col >= 0 and maze[n_row][n_col] == ".":
                     if n_row == 0 or n_row == ROW or n_col == 0 or n_col == COL:
                         return step
