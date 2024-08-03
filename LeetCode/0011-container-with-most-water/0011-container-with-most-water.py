510daaba165a95a91b995199a4787e9145c10f64
Dayong Lee <dayongkr@gmail.com>
2024-08-04 00:36:52 +0900

    Time: 533 ms (28.81%), Space: 29.9 MB (8.57%) - LeetHub

---
+++ LeetCode/0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+
+        result = 0
+
+        while left < right:
+            height_left = height[left]
+            height_right = height[right]
+            result = max(result, min(height_left, height_right) * (right - left))
+
+            if height_left > height_right:
+                right -= 1
+            else:
+                left += 1
+
+        return result
