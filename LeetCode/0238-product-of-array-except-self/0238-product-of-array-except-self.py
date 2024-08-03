598053f89be38d8ce2878d9b59fefc825fc0b275
Dayong Lee <dayongkr@gmail.com>
2024-08-03 21:42:12 +0900

    Time: 273 ms (44.96%), Space: 25.7 MB (76.94%) - LeetHub

--- LeetCode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ LeetCode/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,17 +3,16 @@ from functools import reduce
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [1]
-        suffix = [1]
+        result = [1]
         n = len(nums)
 
-        for i in range(n):
-            prefix.append(nums[i] * prefix[i])
-            suffix.append(nums[n - i - 1] * suffix[i])
+        for i in range(n - 1):
+            result.append(nums[i] * result[i])
 
-        result = []
+        num = nums[n - 1]
 
-        for i in range(n):
-            result.append(prefix[i] * suffix[n - i - 1])
+        for i in range(n - 2, -1, -1):
+            result[i] *= num
+            num *= nums[i]
         
         return result
