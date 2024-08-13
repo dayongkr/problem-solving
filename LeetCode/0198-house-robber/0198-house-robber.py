b566b18de125de90c61f2a445eb948050fbb766b
Dayong Lee <dayongkr@gmail.com>
2024-08-13 23:09:50 +0900

    Time: 29 ms (91.87%), Space: 16.4 MB (66.01%) - LeetHub

--- LeetCode/0198-house-robber/0198-house-robber.py
+++ LeetCode/0198-house-robber/0198-house-robber.py
@@ -3,7 +3,8 @@ class Solution:
         n = len(nums)
         dp = [0 for _ in range(n + 1)]
         dp[1] = nums[0]
+
         for i in range(1, n):
             dp[i + 1] = max(dp[i - 1] + nums[i], dp[i - 2] + nums[i])
 
-        return max(dp)
+        return max(dp[n], dp[n-1])
