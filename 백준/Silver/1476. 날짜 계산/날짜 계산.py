d2cfe1b7780f04a198cbef5235928b476523a1cb
Dayong Lee <dayongkr@gmail.com>
2024-08-27 15:48:31 +0900

    [Silver V] Title: 날짜 계산, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,12 @@
+import math
+import sys
+
+input = sys.stdin.readline
+
+E, S, M = map(int, input().split())
+year = 1
+
+while (year - E) % 15 or (year - S) % 28 or (year - M) % 19:
+    year += 1
+
+print(year)
