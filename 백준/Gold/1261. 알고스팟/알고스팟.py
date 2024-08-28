f345784f101aaa778245cb8e426aad9eebd70dfd
Dayong Lee <dayongkr@gmail.com>
2024-08-28 14:00:36 +0900

    [Gold IV] Title: 알고스팟, Time: 80 ms, Memory: 34848 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,31 +1,27 @@
 import sys
 from collections import deque
+import heapq
 
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 miro = [list(input().rstrip()) for _ in range(M)]
-queue = deque([(0, 0, 0)])
+heap = [(0, 0, 0)]
 visited = {(0, 0): 0}
 d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-result = 1e9
 
-if N == 1 and M == 1:
-    result = 0
-
-while queue:
-    r, c, broken = queue.popleft()
+while heap:
+    b, r, c = heapq.heappop(heap)
+    if r == M - 1 and c == N - 1:
+        print(b)
+        break
 
     for dr, dc in d:
-        nr, nc, nb = r + dr, c + dc, broken
-        if nr == M - 1 and nc == N - 1:
-            result = min(result, nb)
-        elif 0 <= nr < M and 0 <= nc < N:
+        nr, nc, nb = r + dr, c + dc, b
+        if 0 <= nr < M and 0 <= nc < N:
             if miro[nr][nc] == '1':
                 nb += 1
 
             if visited.get((nr, nc), 1e9) > nb:
+                heapq.heappush(heap, (nb, nr, nc))
                 visited[(nr, nc)] = nb
-                queue.append((nr, nc, nb))
-
-print(result)
