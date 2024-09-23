5e162ca0bf10312193554c5c7e9d92cdde5363cb
Dayong Lee <dayongkr@gmail.com>
2024-09-23 12:37:49 +0900

    [level 2] Title: 전화번호 목록, Time: 1487.45 ms, Memory: 284 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,44 @@
-function solution(phoneBook) {
-    return !phoneBook.sort().some((t,i)=> {
-        if(i === phoneBook.length -1) return false;
+class Node {
+    isEnd = false
+    children = new Map()
+}
 
-        return phoneBook[i+1].startsWith(phoneBook[i]);        
-    })
+class Trie {
+    constructor() {
+        this.root = new Node()
+    }
+
+    insert(string) {
+        let cur = this.root
+
+        for (const char of string) {
+            if (!cur.children.has(char)) {
+                cur.children.set(char, new Node())
+            }
+
+            cur = cur.children.get(char)
+
+            if (cur.isEnd){
+                return true
+            }
+        }
+
+        cur.isEnd = true
+
+        return Boolean(cur.children.size)
+    }
+
+
+}
+
+function solution(phone_book) {
+    const trie = new Trie()
+
+    for (const item of phone_book) {
+        if (trie.insert(item)) {
+            return false
+        }
+    }
+
+    return true
 }
