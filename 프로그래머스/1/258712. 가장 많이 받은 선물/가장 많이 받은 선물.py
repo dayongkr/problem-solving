f8fca677d60906570eb08504899b5c01c7ff3942
Dayong Lee <dayongkr@gmail.com>
2024-09-24 23:34:46 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 4.12 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,38 +1,20 @@
-from itertools import combinations
-
 def solution(friends, gifts):
-    gifts_map = []
-    friends_map = {}
-    gifts_score = []
-    result = []
-    
-    for index, friend in enumerate(friends):
-        friends_map[friend] = index
-        gifts_map.append([0 for _ in range(len(friends))])
-        gifts_score.append(0)
-        result.append(0)
-        
-    for gift in gifts:
-        A, B = gift.split()
-        A_index, B_index = friends_map[A], friends_map[B]
-        
-        gifts_map[A_index][B_index] += 1
-        gifts_score[A_index] += 1
-        gifts_score[B_index] -= 1
-    
-    for A_index, B_index in combinations(range(len(friends)),2):
-        if A_index == B_index:
-            continue
-        A_gifts, B_gifts = gifts_map[A_index][B_index], gifts_map[B_index][A_index]
-        A_score, B_score = gifts_score[A_index], gifts_score[B_index]
+    f = {v: i for i, v in enumerate(friends)}
+    l = len(friends)
+    p = [0] * l
+    answer = [0] * l
+    gr = [[0] * l for i in range(l)]
+    for i in gifts:
+        a, b = i.split()
+        gr[f[a]][f[b]] += 1
+    for i in range(l):
+        p[i] = sum(gr[i]) - sum([k[i] for k in gr])
 
-        if A_gifts > B_gifts:
-            result[A_index] += 1
-        elif A_gifts < B_gifts:
-            result[B_index] += 1
-        elif A_score > B_score:
-            result[A_index] += 1
-        elif A_score < B_score:
-            result[B_index] += 1
-    
-    return max(result)
+    for i in range(l):
+        for j in range(l):
+            if gr[i][j] > gr[j][i]:
+                answer[i] += 1
+            elif gr[i][j] == gr[j][i]:
+                if p[i] > p[j]:
+                    answer[i] += 1
+    return max(answer)
