70a0ad019c2a4e6c011e2e0c23628978b0544fbf
Dayong Lee <dayongkr@gmail.com>
2024-09-25 13:46:38 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,3 @@
 function solution(array, commands) {
-    const result = new Array(commands.length)
-    for (let index = 0; index < commands.length; index++) {
-        const [i, j, k] = commands[index]
-        result[index] = array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
-    }
-    return result
+    return commands.map(([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1])
 }
