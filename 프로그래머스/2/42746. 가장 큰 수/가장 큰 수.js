0c5894189c7d50df05e15cd896d91c9eb20b37b3
Dayong Lee <dayongkr@gmail.com>
2024-09-25 23:05:52 +0900

    [level 2] Title: 가장 큰 수, Time: 169.30 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,19 +1,5 @@
 function solution(numbers) {
-    const result = numbers.map(String).sort((a,b) => {
-        if (a + b > b + a) {
-            return -1;
-        }
-        
-        if (a + b < b + a) {
-            return 1
-        }
-        
-        return 0
-    }).join('')
+    const result = numbers.map(String).sort((a,b) => Number(b + a) - Number(a + b)).join('')
     
-    if (result[0] === '0') {
-        return '0'
-    }
-    
-    return result
+    return result[0] === '0' ? '0' : result
 }
