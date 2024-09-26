822be6b3611290401bfd2fbea68bc5b186e142fe
Dayong Lee <dayongkr@gmail.com>
2024-09-26 13:13:10 +0900

    [level 1] Title: 모의고사, Time: 2.61 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,26 +1,26 @@
 function solution(answers) {
-    const answer = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+    const first = [1,2,3,4,5]
+    const second = [2,1,2,3,2,4,2,5]
+    const third = [3,3,1,1,2,2,4,4,5,5]
     
-    const count = [0, 0, 0]
-    let result = []
-    let max = 0
+    const corrected_first = answers.filter((answer, index) => answer === first[index % 5])
+    const corrected_second = answers.filter((answer, index) => answer === second[index % 8])
+    const corrected_third = answers.filter((answer, index) => answer === third[index % 10])
     
-    for (let i = 0; i < answers.length; i++) {
-        for (let j = 0; j < 3; j++) {
-            if (answer[j][i % answer[j].length] === answers[i]) {
-                count[j] += 1
-            }
-        }
+    const max = Math.max(corrected_first.length, corrected_second.length, corrected_third.length)
+    
+    const result = []
+    
+    if (corrected_first.length === max) {
+        result.push(1)
     }
     
-    for (let i = 0; i < 3; i++) {
-        if (count[i] > max) {
-            max = count[i]
-            result = [i + 1]
-        } else if (count[i] === max) {
-            result.push(i + 1)
-        }
+    if (corrected_second.length === max) {
+        result.push(2)
     }
     
+    if (corrected_third.length === max) {
+        result.push(3)
+    }
     return result;
 }
