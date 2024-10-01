c086d85f5ce8eb7a4f13bd7f75ae9bbd14f5b62e
Dayong Lee <dayongkr@gmail.com>
2024-10-01 17:20:54 +0900

    [Gold V] Title: 토마토, Time: 2352 ms, Memory: 108192 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -3,38 +3,38 @@ from collections import deque
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split(" "))
-graph = [list(map(int, input().split())) for _ in range(M)]
-out = [[0 for _ in range(N)] for _ in range(M)]
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+M, N = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+d = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+visited = [[False for _ in range(M)] for _ in range(N)]
 queue = deque()
-max_t_count = 0
-t_count = 0
-day_count = -1
+count = 0  # 익기 전 토마토 갯수
+result = 0
+
 
 for i in range(N):
     for j in range(M):
-        if graph[j][i] == -1:
-            continue
-        elif graph[j][i] == 1:
-            queue.append((i, j))
-            out[j][i] = 1
-        max_t_count += 1
+        cur = graph[i][j]
+        if cur == 1:
+            queue.append((i, j, 0))
+            visited[i][j] = True
+        if cur == 0:
+            count += 1
 
 while queue:
-    t_count += len(queue)
-    temp = [queue.popleft() for _ in range(len(queue))]
-    day_count += 1
-    for x, y in temp:
-        for i in range(4):
-            nx = dx[i] + x
-            ny = dy[i] + y
-            if N > nx >= 0 and M > ny >= 0 and not out[ny][nx] and graph[ny][nx] != -1:
-                queue.append((nx, ny))
-                out[ny][nx] = 1
+    r, c, day = queue.popleft()
 
-if t_count == max_t_count:
-    print(day_count)
-else:
-    print(-1)
+    for dr, dc in d:
+        nr, nc, nday = r + dr, c + dc, day + 1
+
+        if N > nr >= 0 and M > nc >= 0 and not visited[nr][nc] and graph[nr][nc] == 0:
+            count -= 1
+            result = max(result, nday)
+            queue.append((nr, nc, nday))
+            visited[nr][nc] = True
+
+if count != 0:  # 다 익지 못하면 -1
+    result = -1
+
+print(result)
