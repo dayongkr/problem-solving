c401f0fa71b4d29f9b393f2faf49b9c5c3749fe2
Dayong Lee <dayongkr@gmail.com>
2024-10-03 20:07:02 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+arr = [0 for _ in range(M)]
+visited = [False for _ in range(N)]
+
+nums.sort()
+
+
+def rec(m):
+    global N, M, visited
+
+    if m == M:
+        for i in range(M):
+            print(arr[i], end=" ")
+        print()
+        return
+
+    for i in range(N):
+        if visited[i]:
+            continue
+
+        arr[m] = nums[i]
+        visited[i] = True
+        rec(m + 1)
+        visited[i] = False
+
+
+rec(0)
