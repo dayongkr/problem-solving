8d7213930e214c7214c441de88d69933d62aca0e
Dayong Lee <dayongkr@gmail.com>
2024-10-03 21:52:21 +0900

    [Gold III] Title: 감시, Time: 1388 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -11,10 +11,50 @@ cameras = []
 camera3 = [(3, 0), (0, 1), (1, 2), (2, 3)]
 directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
+# CCTV가 감시할 수 있는 방향 미리 계산
+def watch_area(r, c, d, visited):
+    while True:
+        nr, nc = r + d[0], c + d[1]
+        if not (0 <= nr < N and 0 <= nc < M) or graph[nr][nc] == 6:
+            break
+        if graph[nr][nc] == 0 and not visited[nr][nc]:
+            visited[nr][nc] = True
+        r, c = nr, nc
+
+# 감시 영역 미리 계산
+def calc_watch(visited, brute):
+    for cameraR, cameraC, cameraType in cameras:
+        rotation = brute % 4
+        brute //= 4
+
+        if cameraType == 1:
+            watch_area(cameraR, cameraC, directions[rotation], visited)
+        elif cameraType == 2:
+            watch_area(cameraR, cameraC, directions[rotation], visited)
+            watch_area(cameraR, cameraC, directions[(rotation + 2) % 4], visited)
+        elif cameraType == 3:
+            watch_area(cameraR, cameraC, directions[camera3[rotation][0]], visited)
+            watch_area(cameraR, cameraC, directions[camera3[rotation][1]], visited)
+        elif cameraType == 4:
+            for dIndex in range(4):
+                if dIndex != rotation:
+                    watch_area(cameraR, cameraC, directions[dIndex], visited)
+        else:  # type 5
+            for direction in directions:
+                watch_area(cameraR, cameraC, direction, visited)
+
+# 감시하지 않는 영역 카운트
+def count_unwatched(visited):
+    unwatched = 0
+    for r in range(N):
+        for c in range(M):
+            if graph[r][c] == 0 and not visited[r][c]:
+                unwatched += 1
+    return unwatched
+
 for r in range(N):
     for c in range(M):
         cur = graph[r][c]
-
         if cur == 0:
             spaces += 1
         elif cur != 6:
@@ -24,46 +64,8 @@ cases = 4 ** len(cameras)
 answer = spaces
 
 for i in range(cases):
-    brute = i
-    queue = deque()
     visited = [[False for _ in range(M)] for _ in range(N)]
-    result = spaces
-
-    for cameraR, cameraC, cameraType in cameras:
-        rotation = brute % 4
-
-        if cameraType == 1:
-            queue.append((cameraR, cameraC, directions[rotation]))
-        elif cameraType == 2:
-            queue.append((cameraR, cameraC, directions[rotation]))
-            queue.append(
-                (cameraR, cameraC, directions[rotation - 2 if rotation >= 2 else rotation + 2]))
-        elif cameraType == 3:
-            queue.append((cameraR, cameraC, directions[camera3[rotation][0]]))
-            queue.append((cameraR, cameraC, directions[camera3[rotation][1]]))
-        elif cameraType == 4:
-            for dIndex in range(4):
-                if dIndex == rotation:
-                    continue
-                queue.append((cameraR, cameraC, directions[dIndex]))
-        else:
-            for direction in directions:
-                queue.append((cameraR, cameraC, direction))
-
-        visited[cameraR][cameraC] = True
-
-        while queue:
-            r, c, d = queue.pop()
-            nr, nc = r + d[0], c+d[1]
-
-            if N > nr >= 0 and M > nc >= 0 and graph[nr][nc] != 6:
-                if graph[nr][nc] == 0 and not visited[nr][nc]:
-                    result -= 1
-                    visited[nr][nc] = True
-                queue.append((nr, nc, d))
-
-        brute //= 4
-
-    answer = min(result, answer)
+    calc_watch(visited, i)
+    answer = min(answer, count_unwatched(visited))
 
 print(answer)
